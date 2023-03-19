a4ec67bcf6324217c4884401cc9103643da1f9d3
Confidence Madiope <confidencem97@gmail.com>
2023-03-19 12:40:06 +0200

    ADD:useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import BlogContent from "./pages/BlogContent";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+  let { loading, error, data } = useFetch(
+    "http://localhost:1337/api/blogs?popular=*"
+  );
+  if (loading) return <h1>Loading ...</h1>;
+  if (error) return <h1>Error ...</h1>;
+
+  console.log(data);
+
   return (
     <div>
       <Routes>
